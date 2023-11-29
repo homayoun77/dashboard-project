c799e8fc45f572b6cbf5b7518b6a5d0f5cb1ef21
Homayoun Norozy <homayounn77@gmail.com>
2023-11-29 17:41:10 +0330

    c

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://homayoun77.github.io/dashboard-project",
+  base: "/dashboard-project",
   plugins: [react()],
 })
